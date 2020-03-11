6452645c4f870a1fae16b5ad118c03195cd8e3a3
Prabesh Dhakal <prfrbest@gmail.com>
2020-03-11 23:15:19 +0100

    Update Code for Cluster Data_1.R

--- ML/Code for Cluster Data_1.R
+++ ML/Code for Cluster Data_1.R
@@ -1,9 +1,16 @@
-n = 150*2
+dims = 2
 k = 3
 
 set.seed(42)
-x = matrix(rnorm(n, 10), 150, 2)
-clusters = sample(1:3, 150, replace=TRUE)
-centroids = matrix(rnorm(3*2, mean=1, sd=10), 3, 2)
-clustered_x = x + centroids[clusters]
-plot(clustered_x, col=clusters)
+
+n = 150
+xs = matrix(rnorm(n*dims, 10), n, dims)
+clusters = sample(1:k, n, replace=TRUE)
+centroids = matrix(rnorm(k*dims, mean=1, sd=10), k, dims)
+clustered_x = cbind(xs + centroids[clusters], clusters)
+if (dim(clustered_x)==2){
+    colnames(clustered_x) = c("x1", "x2", "cluster")
+}
+head(clustered_x)
+
+plot(clustered_x, col=clustered_x[,3], pch=19)
